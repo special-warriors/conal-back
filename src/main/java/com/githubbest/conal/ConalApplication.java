4198a6bf5ebb2ca29276efe19b4c60fa301f4c7c
dnjstjt1297 <dnjstjt1297@gmail.com>
2025-05-01 17:08:31 +0900

    init: create project

---
+++ src/main/java/com/githubbest/conal/ConalApplication.java
@@ -0,0 +1,13 @@
+package com.githubbest.conal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/githubbest/conal/ConalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.githubbest.conal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
