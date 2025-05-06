6d5597c3507ce13488833cff7975805045110ff7
dnjstjt1297 <dnjstjt1297@gmail.com>
2025-05-07 00:31:37 +0900

    feat: 깃 레포 예외 코드 추가 (#4)

--- src/main/java/com/specialwarriors/conal/github_repo/exception/GithubRepoException.java
+++ src/main/java/com/specialwarriors/conal/github_repo/exception/GithubRepoException.java
@@ -9,8 +9,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum GithubRepoException implements BaseException {
 
-    UNAUTHORIZED_REPO_ACCESS(HttpStatus.BAD_REQUEST, "사용자는 자신의 repo에만 접근할 수 있습니다.");
-
+    UNAUTHORIZED_GITHUBREPO_ACCESS(HttpStatus.FORBIDDEN, "리포지토리 접근 권한이 없습니다."),
+    NOTFOUND_GITHUBREPO(HttpStatus.NOT_FOUND, "깃허브 리포지토리를 찾을 수 없습니다.");
     private final HttpStatus status;
     private final String message;
 }
