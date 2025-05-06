6efdab6f202dfe1a08b2a89ea21349c2f257441d
dnjstjt1297 <dnjstjt1297@gmail.com>
2025-05-07 00:35:17 +0900

    feat: contributor domain 추가 (#4)

---
+++ src/main/java/com/specialwarriors/conal/contributor/domain/Contributor.java
@@ -0,0 +1,48 @@
+package com.specialwarriors.conal.contributor.domain;
+
+import com.specialwarriors.conal.github_repo.domain.GithubRepo;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PACKAGE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Contributor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "contributor_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "github_repo_id")
+    private GithubRepo githubRepo;
+
+    public static Contributor of(String email) {
+        return Contributor.builder()
+            .email(email)
+            .build();
+    }
+
+    // 연관관계 메서드
+    public void setGithubRepo(GithubRepo repo) {
+        this.githubRepo = repo;
+    }
+
+}
