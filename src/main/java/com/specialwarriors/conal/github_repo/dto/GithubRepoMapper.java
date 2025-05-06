f95b7839fd35dd5341a9284a20f55e9b2c94c192
dnjstjt1297 <dnjstjt1297@gmail.com>
2025-05-07 00:34:47 +0900

    feat: 깃 레포 dto 메퍼 추가 mapstruct 활용 (#4)

---
+++ src/main/java/com/specialwarriors/conal/github_repo/dto/GithubRepoMapper.java
@@ -0,0 +1,34 @@
+package com.specialwarriors.conal.github_repo.dto;
+
+
+import com.specialwarriors.conal.github_repo.domain.GithubRepo;
+import com.specialwarriors.conal.github_repo.dto.request.GithubRepoCreateRequest;
+import com.specialwarriors.conal.github_repo.dto.response.GithubRepoCreateResponse;
+import com.specialwarriors.conal.github_repo.dto.response.GithubRepoGetResponse;
+import com.specialwarriors.conal.github_repo.dto.response.GithubRepoPageResponse;
+import com.specialwarriors.conal.github_repo.dto.response.GithubRepoPageResponse.GithubRepoSummary;
+import org.mapstruct.Mapper;
+import org.springframework.data.domain.Page;
+
+@Mapper(componentModel = "spring")
+public interface GithubRepoMapper {
+
+    GithubRepo toGithubRepo(GithubRepoCreateRequest request);
+
+    GithubRepoCreateResponse toGithubRepoCreateResponse(GithubRepo repo);
+
+    GithubRepoGetResponse toGithubRepoGetResponse(GithubRepo repo);
+
+    GithubRepoSummary toGithubRepoSummary(GithubRepo repo);
+
+    default GithubRepoPageResponse toGithubRepoPageResponse(Page<GithubRepo> page) {
+        return new GithubRepoPageResponse(
+            page.getContent().stream()
+                .map(this::toGithubRepoSummary)
+                .toList(),
+            page.getNumber(),
+            page.getTotalPages(),
+            page.getTotalElements()
+        );
+    }
+}
