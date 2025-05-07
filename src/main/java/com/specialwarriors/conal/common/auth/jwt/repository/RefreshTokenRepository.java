77cf474def18c6b70c939373a28ae3f4325c32ec
yereumi <lovelyyeji01@gmail.com>
2025-05-07 14:44:53 +0900

    feat: findRefreshTokenByUserId 메서드 작성(#3)

---
+++ src/main/java/com/specialwarriors/conal/common/auth/jwt/repository/RefreshTokenRepository.java
@@ -0,0 +1,10 @@
+package com.specialwarriors.conal.common.auth.jwt.repository;
+
+import com.specialwarriors.conal.common.auth.jwt.domain.RefreshToken;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, String> {
+
+    Optional<RefreshToken> findRefreshTokenByUserId(Long userId);
+}
