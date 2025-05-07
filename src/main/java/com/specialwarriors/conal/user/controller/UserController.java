2c99e5839f5bbfb7501f07f656d9a4f28c6a896a
yereumi <lovelyyeji01@gmail.com>
2025-05-07 14:43:49 +0900

    feat: 임시 UserController 구현(#3)

---
+++ src/main/java/com/specialwarriors/conal/user/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.specialwarriors.conal.user.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@Controller
+public class UserController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
+
+}
