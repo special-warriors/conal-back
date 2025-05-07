b26a98247089c102af64c2706b9198ce7ca77ab9
yereumi <lovelyyeji01@gmail.com>
2025-05-07 17:01:52 +0900

    feat: repository 어노테이션 추가(#3)

--- src/main/java/com/specialwarriors/conal/common/auth/jwt/repository/RefreshTokenRepository.java
+++ src/main/java/com/specialwarriors/conal/common/auth/jwt/repository/RefreshTokenRepository.java
@@ -3,7 +3,9 @@ package com.specialwarriors.conal.common.auth.jwt.repository;
 import com.specialwarriors.conal.common.auth.jwt.domain.RefreshToken;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
 
     Optional<RefreshToken> findRefreshTokenByUserId(Long userId);
