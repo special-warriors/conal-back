6695e1247927ceaea48cfc67d86333f5b2df2ed9
dnjstjt1297 <dnjstjt1297@gmail.com>
2025-05-08 15:29:23 +0900

    feat: 깃 레포 이메일 NOT_FOUND 예외 추가 (#4)

--- src/main/java/com/specialwarriors/conal/github_repo/exception/GithubRepoException.java
+++ src/main/java/com/specialwarriors/conal/github_repo/exception/GithubRepoException.java
@@ -10,7 +10,9 @@ import org.springframework.http.HttpStatus;
 public enum GithubRepoException implements BaseException {
 
     UNAUTHORIZED_GITHUBREPO_ACCESS(HttpStatus.FORBIDDEN, "리포지토리 접근 권한이 없습니다"),
-    NOT_FOUND_GITHUBREPO(HttpStatus.NOT_FOUND, "깃허브 리포지토리를 찾을 수 없습니다");
+    NOT_FOUND_GITHUBREPO(HttpStatus.NOT_FOUND, "깃허브 리포지토리를 찾을 수 없습니다"),
+    NOT_FOUND_GITHUBEMAIL(HttpStatus.NOT_FOUND, "기여자 이메일이 없습니다");
+
     private final HttpStatus status;
     private final String message;
 }
