83cc9d104d47802ec42ce53a315a01f4a59c97e2
yereumi <lovelyyeji01@gmail.com>
2025-05-08 09:05:51 +0900

    fix: 로그에 클래스명 하드코딩 수정(#3)

--- src/main/java/com/specialwarriors/conal/common/auth/service/AuthService.java
+++ src/main/java/com/specialwarriors/conal/common/auth/service/AuthService.java
@@ -1,5 +1,6 @@
 package com.specialwarriors.conal.common.auth.service;
 
+import com.specialwarriors.conal.common.LogService;
 import com.specialwarriors.conal.common.auth.exception.AuthException;
 import com.specialwarriors.conal.common.auth.exception.CustomAuthException;
 import com.specialwarriors.conal.common.auth.jwt.JwtProvider;
@@ -17,6 +18,7 @@ public class AuthService {
 
     private final JwtProvider jwtProvider;
     private final RefreshTokenRepository refreshTokenRepository;
+    private final LogService logService;
 
     public JwtTokenResponse reissueToken(String refreshToken) {
         Long userId = jwtProvider.getUserId(refreshToken);
@@ -35,14 +37,14 @@ public class AuthService {
             String newRefreshToken = jwtProvider.createRefreshToken(userId);
 
             savedToken.updateRefreshToken(newRefreshToken);
-            refreshTokenRepository.save(savedToken);
 
-            log.info("[AuthService] accessToken과 refreshToken을 재발행했습니다");
+            log.info("[{}}] accessToken과 refreshToken을 재발행했습니다", logService.getClassName());
             return new JwtTokenResponse(newAccessToken, newRefreshToken);
         }
 
         String newAccessToken = jwtProvider.createAccessToken(userId);
-        log.info("[AuthService] accessToken을 재발행했습니다");
+        log.info("[{}] accessToken을 재발행했습니다", logService.getClassName());
         return new JwtTokenResponse(newAccessToken, refreshToken);
     }
+
 }
