8c7d59153c1c10d66447f4ffcd783a43e2ff740d
minjae <mj3242@naver.com>
2025-05-09 13:28:48 +0900

    feat: ID 기반 github repo 조회 로직 추가(#7)

--- src/main/java/com/specialwarriors/conal/github_repo/service/GithubRepoQuery.java
+++ src/main/java/com/specialwarriors/conal/github_repo/service/GithubRepoQuery.java
@@ -25,4 +25,10 @@ public class GithubRepoQuery {
 
         return githubRepo;
     }
+
+    public GithubRepo findByRepositoryId(long repositoryId) {
+
+        return githubRepoRepository.findById(repositoryId)
+                .orElseThrow(() -> new GeneralException(GithubRepoException.NOT_FOUND_GITHUBREPO));
+    }
 }
