608aa5eeb90aadb7803639f967d7b7231b28e3b2
dnjstjt1297 <dnjstjt1297@gmail.com>
2025-05-09 11:00:48 +0900

    feat: 필요없는 dto 삭제 (#11)

--- src/main/java/com/specialwarriors/conal/feat/github/dto/CommitRank.java
+++
@@ -1,7 +0,0 @@
-package com.specialwarriors.conal.feat.github.dto;
-
-public record CommitRank(String username,
-                            long rank,
-                            int commits) {
-
-}
--- src/main/java/com/specialwarriors/conal/feat/github/dto/response/CommitCountResponse.java
+++
@@ -1,5 +0,0 @@
-package com.specialwarriors.conal.feat.github.dto.response;
-
-public record CommitCountResponse(String githubUsername, int count) {
-
-}
