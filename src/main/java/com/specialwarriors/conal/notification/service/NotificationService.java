fe0f9f6a13c7675814629c1316dd6c99d4dc4b20
dnjstjt1297 <dnjstjt1297@gmail.com>
2025-05-10 22:30:04 +0900

    feat: 깃 레포 객체를 통해 NotificationAgreement 엔티티를 가져오는 기능으로 변경 (#11)

--- src/main/java/com/specialwarriors/conal/notification/service/NotificationService.java
+++ src/main/java/com/specialwarriors/conal/notification/service/NotificationService.java
@@ -1,7 +1,9 @@
 package com.specialwarriors.conal.notification.service;
 
 import com.specialwarriors.conal.common.exception.GeneralException;
+import com.specialwarriors.conal.github_repo.domain.GithubRepo;
 import com.specialwarriors.conal.github_repo.exception.GithubRepoException;
+import com.specialwarriors.conal.github_repo.service.GithubRepoQuery;
 import com.specialwarriors.conal.notification.domain.NotificationAgreement;
 import com.specialwarriors.conal.notification.dto.request.NotificationAgreementUpdateRequest;
 import com.specialwarriors.conal.notification.enums.NotificationType;
@@ -17,14 +19,17 @@ public class NotificationService {
 
     private final UserQuery userQuery;
     private final NotificationAgreementQuery notificationAgreementQuery;
+    private final GithubRepoQuery githubRepoQuery;
 
     @Transactional
     public void updateNotificationAgreement(long userId, long repositoryId,
         NotificationAgreementUpdateRequest request) {
 
+        GithubRepo githubRepo = githubRepoQuery.findByRepositoryId(repositoryId);
+
         NotificationType notificationType = NotificationType.valueOf(request.type());
         NotificationAgreement notificationAgreement = notificationAgreementQuery
-            .findByRepositoryIdAndType(repositoryId, notificationType);
+            .findByGithubRepoAndType(githubRepo, notificationType);
 
         // 사용자가 자신의 github repo에 접근한 것이 맞는 지 검증
         User user = userQuery.findById(userId);
