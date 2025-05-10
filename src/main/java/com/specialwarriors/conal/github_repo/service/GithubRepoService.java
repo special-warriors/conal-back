34576fa1e6a7d6fff129d4f43a689f742725d2c8
dnjstjt1297 <dnjstjt1297@gmail.com>
2025-05-11 00:40:07 +0900

    feat: 깃 레포 조회 시 url을 통해 owner와 repo이름 파싱 로직 추가 (#11)

--- src/main/java/com/specialwarriors/conal/github_repo/service/GithubRepoService.java
+++ src/main/java/com/specialwarriors/conal/github_repo/service/GithubRepoService.java
@@ -20,6 +20,8 @@ import com.specialwarriors.conal.user.domain.User;
 import com.specialwarriors.conal.user.service.UserQuery;
 import java.util.List;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -40,12 +42,12 @@ public class GithubRepoService {
 
     private final UserQuery userQuery;
     private final GithubRepoQuery githubRepoQuery;
-
     private final GithubRepoMapper githubRepoMapper;
 
     @Transactional
     public GithubRepoCreateResponse createGithubRepo(Long userId, GithubRepoCreateRequest request) {
         User user = userQuery.findById(userId);
+        validateGitHubUrl(request.url());
 
         List<Contributor> contributors = createAndSaveContributors(request.emails());
 
@@ -64,6 +66,14 @@ public class GithubRepoService {
         return githubRepoMapper.toGithubRepoCreateResponse(githubRepo);
     }
 
+    private void validateGitHubUrl(String url) {
+        // GitHub 저장소 URL 형식 검증: https://github.com/{owner}/{repo}
+        String regex = "^https://github\\.com/[^/]+/[^/]+$";
+        if (!url.matches(regex)) {
+            throw new GeneralException(GithubRepoException.INVALID_URL);
+        }
+    }
+
     private List<Contributor> createAndSaveContributors(Set<String> emails) {
         if (emails.isEmpty()) {
             throw new GeneralException(GithubRepoException.NOT_FOUND_GITHUBEMAIL);
@@ -79,7 +89,16 @@ public class GithubRepoService {
 
         GithubRepo githubRepo = githubRepoQuery.findByUserIdAndRepositoryId(userId, repoId);
 
-        return githubRepoMapper.toGithubRepoGetResponse(githubRepo);
+        Pattern pattern = Pattern.compile("https://github\\.com/([^/]+)/([^/]+)");
+        Matcher matcher = pattern.matcher(githubRepo.getUrl());
+        if (matcher.find()) {
+            String owner = matcher.group(1);
+            String repo = matcher.group(2);
+
+            return githubRepoMapper.toGithubRepoGetResponse(githubRepo, owner, repo);
+        } else {
+            throw new GeneralException(GithubRepoException.NOT_FOUND_GITHUBREPO);
+        }
     }
 
     @Transactional(readOnly = true)
