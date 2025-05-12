26b059568da8a39f907b9fd7e1a4eca7586e3889
dnjstjt1297 <dnjstjt1297@gmail.com>
2025-05-12 15:17:30 +0900

    feat: 필요한 변수 추가 및 사용하지 않는 변수 삭제 (#11)

--- src/main/java/com/specialwarriors/conal/github_repo/dto/response/GithubRepoCreateResponse.java
+++ src/main/java/com/specialwarriors/conal/github_repo/dto/response/GithubRepoCreateResponse.java
@@ -1,11 +1,8 @@
 package com.specialwarriors.conal.github_repo.dto.response;
 
-import java.time.LocalDate;
-
 public record GithubRepoCreateResponse(
-    String name,
-    String url,
-    LocalDate endDate
+    String owner,
+    String repo
 ) {
 
 }
