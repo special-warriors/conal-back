6b82960215f10c337648c6115ebca3fe61169c43
yereumi <lovelyyeji01@gmail.com>
2025-05-12 09:06:37 +0900

    feat: 로그인 성공 시 깃허브 토큰 redis에 저장하도록 추가(#12)

--- src/main/java/com/specialwarriors/conal/common/auth/oauth/CustomOAuth2SuccessHandler.java
+++ src/main/java/com/specialwarriors/conal/common/auth/oauth/CustomOAuth2SuccessHandler.java
@@ -8,8 +8,13 @@ import com.specialwarriors.conal.user.repository.UserRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.Duration;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
@@ -18,6 +23,8 @@ public class CustomOAuth2SuccessHandler implements AuthenticationSuccessHandler 
 
     private final UserRepository userRepository;
     private final SessionManager sessionManager;
+    private final RedisTemplate<String, String> redisTemplate;
+    private final OAuth2AuthorizedClientService authorizedClientService;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request,
@@ -30,6 +37,18 @@ public class CustomOAuth2SuccessHandler implements AuthenticationSuccessHandler 
         User user = userRepository.findByGithubId(githubId)
                 .orElseThrow(() -> new GeneralException(UserException.USER_NOT_FOUND));
         sessionManager.createSession(request, user.getId());
+
+        OAuth2AuthenticationToken oauthToken = (OAuth2AuthenticationToken) authentication;
+        String registrationId = oauthToken.getAuthorizedClientRegistrationId();
+
+        OAuth2AuthorizedClient client = authorizedClientService.loadAuthorizedClient(
+                registrationId,
+                oauthToken.getName()
+        );
+
+        String accessToken = client.getAccessToken().getTokenValue();
+        String redisKey = "github:token:" + githubId;
+        redisTemplate.opsForValue().set(redisKey, accessToken, Duration.ofHours(1));
 
         response.sendRedirect("/login/success");
     }
