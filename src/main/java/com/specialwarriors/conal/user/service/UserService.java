167c5f07b4612b774214417386bf6f15e5863f2c
yereumi <lovelyyeji01@gmail.com>
2025-05-12 16:37:46 +0900

    fix: Optional 제거(#12)

--- src/main/java/com/specialwarriors/conal/user/service/UserService.java
+++ src/main/java/com/specialwarriors/conal/user/service/UserService.java
@@ -5,7 +5,6 @@ import com.specialwarriors.conal.common.exception.GeneralException;
 import com.specialwarriors.conal.user.domain.User;
 import com.specialwarriors.conal.user.exception.UserException;
 import com.specialwarriors.conal.user.repository.UserRepository;
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
@@ -33,8 +32,7 @@ public class UserService {
                 .get("github:token:" + user.getGithubId());
 
         githubOAuth2WebClient.unlink(oauthAccessToken);
-        Optional.of(user.getGithubId())
-                .ifPresent(id -> redisTemplate.unlink("github:token:" + id));
+        redisTemplate.unlink("github:token:" + user.getGithubId());
 
         userRepository.deleteById(userId);
     }
