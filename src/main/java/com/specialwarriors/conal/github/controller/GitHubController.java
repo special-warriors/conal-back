ebac2fa45e743391000332f4a21276361631d784
dnjstjt1297 <dnjstjt1297@gmail.com>
2025-05-12 15:12:26 +0900

    feat: 업데이트 url변경 및 리액티브 레디스에서 기여도 조회 기능 추가 (#11)

--- src/main/java/com/specialwarriors/conal/github/controller/GitHubController.java
+++ src/main/java/com/specialwarriors/conal/github/controller/GitHubController.java
@@ -1,12 +1,15 @@
 package com.specialwarriors.conal.github.controller;
 
 import com.specialwarriors.conal.github.service.GitHubService;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -16,13 +19,25 @@ public class GitHubController {
 
     private final GitHubService githubService;
 
-
-    @PostMapping("/repos/{owner}/{repo}/ranking")
-    public Mono<ResponseEntity<String>> updateAllGithubContributorRanks(
+    @GetMapping("/repos/{owner}/{repo}/details")
+    public Mono<Map<String, Map<String, String>>> getContributorDetailsFromRedis(
         @PathVariable String owner,
         @PathVariable String repo
     ) {
-        return githubService.updateRepoRanking(owner, repo)
+        return githubService.getContributorsFromRedis(owner, repo)
+            .flatMapMany(Flux::fromIterable)
+            .flatMap(login ->
+                githubService.getContributorDetailFromRedis(owner, repo, login)
+                    .map(detailMap -> Map.entry(login, detailMap)))
+            .collectMap(Map.Entry::getKey, Map.Entry::getValue);
+    }
+
+    @PostMapping("/repos/{owner}/{repo}/update")
+    public Mono<ResponseEntity<String>> updateAllGithubContributorAndContribution(
+        @PathVariable String owner,
+        @PathVariable String repo
+    ) {
+        return githubService.updateRepoContribution(owner, repo)
             .thenReturn(ResponseEntity.ok("전체 랭킹 업데이트 완료"));
     }
 
