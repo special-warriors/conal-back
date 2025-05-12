ef5d171cfc68474b8d5462902564ea96de83d1b4
dnjstjt1297 <dnjstjt1297@gmail.com>
2025-05-12 15:15:10 +0900

    fix: 유저ID 변수 이름 변경 (#11)

--- src/main/java/com/specialwarriors/conal/github_repo/dto/request/GithubRepoCreateRequest.java
+++ src/main/java/com/specialwarriors/conal/github_repo/dto/request/GithubRepoCreateRequest.java
@@ -4,7 +4,7 @@ import java.time.LocalDate;
 import java.util.Set;
 
 public record GithubRepoCreateRequest(
-    Long UserId,
+    Long userId,
     String name,
     String url,
     LocalDate endDate,
