79eed7a674117809192c26e78bce88ecabf02d2a
dnjstjt1297 <dnjstjt1297@gmail.com>
2025-05-13 09:33:49 +0900

    feat: 수신알림 엔티티와 매핑을 OneToMany로 변경하여 응답 dto 변경(#9)

--- src/main/java/com/specialwarriors/conal/github_repo/dto/response/GithubRepoGetResponse.java
+++ src/main/java/com/specialwarriors/conal/github_repo/dto/response/GithubRepoGetResponse.java
@@ -2,11 +2,12 @@ package com.specialwarriors.conal.github_repo.dto.response;
 
 import com.specialwarriors.conal.notification.domain.NotificationAgreement;
 import java.time.LocalDate;
+import java.util.List;
 
 public record GithubRepoGetResponse(
     String name,
     String url,
-    NotificationAgreement agreement,
+    List<NotificationAgreement> agreement,
     LocalDate endDate,
     String owner,
     String repo
