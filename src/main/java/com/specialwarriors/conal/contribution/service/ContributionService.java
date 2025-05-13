6a9809cb9f69d515b3f7e9a9213af3bd644d8f3c
dnjstjt1297 <dnjstjt1297@gmail.com>
2025-05-13 16:22:58 +0900

    feat: 기여도 메일 서비스 (#9)

--- src/main/java/com/specialwarriors/conal/contribution/service/ContributionService.java
+++ src/main/java/com/specialwarriors/conal/contribution/service/ContributionService.java
@@ -1,5 +1,22 @@
 package com.specialwarriors.conal.contribution.service;
 
+import com.specialwarriors.conal.contribution.dto.response.ContributionFormResponse;
+import com.specialwarriors.conal.contributor.domain.Contributor;
+import com.specialwarriors.conal.github_repo.domain.GithubRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class ContributionService {
+
+    public ContributionFormResponse sendEmail(Contributor contributor, GithubRepo githubRepo) {
+        Long userId = githubRepo.getUser().getId();
+        Long repoId = githubRepo.getId();
+
+        return new ContributionFormResponse(userId, repoId,
+            contributor.getEmail());
+    }
+
 
 }
