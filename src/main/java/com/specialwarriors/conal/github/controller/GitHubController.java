52fd80ed9827bde6e01a2137634d98c27b7674d8
dnjstjt1297 <dnjstjt1297@gmail.com>
2025-05-13 16:24:25 +0900

    feat: 커밋내역 기능 삭제 (#9)

--- src/main/java/com/specialwarriors/conal/github/controller/GitHubController.java
+++ src/main/java/com/specialwarriors/conal/github/controller/GitHubController.java
@@ -1,7 +1,6 @@
 package com.specialwarriors.conal.github.controller;
 
 import com.specialwarriors.conal.github.service.GitHubService;
-import java.util.List;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -9,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -43,13 +41,5 @@ public class GitHubController {
             .thenReturn(ResponseEntity.ok("전체 랭킹 업데이트 완료"));
     }
 
-    @GetMapping("/repos/{owner}/{repo}/commits")
-    public Mono<List<Map<String, String>>> getCommits(
-        @PathVariable String owner,
-        @PathVariable String repo,
-        @RequestParam(defaultValue = "1") int page
-    ) {
-        return githubService.getCommits(owner, repo, page);
-    }
 
 }
