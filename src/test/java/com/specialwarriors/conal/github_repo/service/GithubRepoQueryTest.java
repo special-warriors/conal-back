94049abad95c8139b50faf56ac7249c7ccfc4540
minjae <mj3242@naver.com>
2025-05-15 15:47:16 +0900

    test: 사용자 ID, Repository ID 기반 조회 테스트 추가(#31)

--- src/test/java/com/specialwarriors/conal/github_repo/service/GithubRepoQueryTest.java
+++ src/test/java/com/specialwarriors/conal/github_repo/service/GithubRepoQueryTest.java
@@ -8,6 +8,7 @@ import com.specialwarriors.conal.common.exception.GeneralException;
 import com.specialwarriors.conal.github_repo.domain.GithubRepo;
 import com.specialwarriors.conal.github_repo.exception.GithubRepoException;
 import com.specialwarriors.conal.github_repo.repository.GithubRepoRepository;
+import com.specialwarriors.conal.user.exception.UserException;
 import com.specialwarriors.conal.user.repository.UserRepository;
 import com.specialwarriors.conal.user.service.UserQuery;
 import org.junit.jupiter.api.BeforeEach;
@@ -72,6 +73,79 @@ class GithubRepoQueryTest {
                     .isInstanceOf(GeneralException.class)
                     .extracting("exception")
                     .isEqualTo(GithubRepoException.GITHUB_REPO_NOT_FOUND);
+        }
+    }
+
+    @Nested
+    @DisplayName("사용자 ID와 Github Repository ID로 Github Repository를 조회할 때 ")
+    @Sql(scripts = "/sql/github_repo/service/find_by_user_id_and_repository_id_test_setup.sql",
+            executionPhase = ExecutionPhase.BEFORE_TEST_CLASS)
+    class FindByUserIdAndRepositoryIdTest {
+
+        @DisplayName("성공한다.")
+        @Test
+        public void success() {
+            // given
+            long userId = 1L;
+            long repositoryId = 1L;
+
+            // when
+            GithubRepo githubRepo = githubRepoQuery.findByUserIdAndRepositoryId(userId,
+                    repositoryId);
+
+            // then
+            assertThat(githubRepo.getId()).isEqualTo(repositoryId);
+        }
+
+        @DisplayName("존재하지 않는 Github Repository일 경우 예외가 발생한다.")
+        @Test
+        public void githubRepoNotFound() {
+            // given
+            long userId = 1L;
+            long repositoryId = 3L;
+
+            // when
+
+            // then
+            assertThatThrownBy(() ->
+                    githubRepoQuery.findByUserIdAndRepositoryId(userId, repositoryId))
+                    .isInstanceOf(GeneralException.class)
+                    .extracting("exception")
+                    .isEqualTo(GithubRepoException.GITHUB_REPO_NOT_FOUND);
+        }
+
+        @DisplayName("존재하지 않는 사용자일 경우 예외가 발생한다.")
+        @Test
+        public void userNotFound() {
+            // given
+            long userId = 3L;
+            long repositoryId = 1L;
+
+            // when
+
+            // then
+            assertThatThrownBy(
+                    () -> githubRepoQuery.findByUserIdAndRepositoryId(userId, repositoryId))
+                    .isInstanceOf(GeneralException.class)
+                    .extracting("exception")
+                    .isEqualTo(UserException.USER_NOT_FOUND);
+        }
+
+        @DisplayName("사용자의 Github Repository가 아닐 경우 예외가 발생한다.")
+        @Test
+        public void unauthorizedGithubRepoAccess() {
+            // given
+            long userId = 1L;
+            long repositoryId = 2L;
+
+            // when
+
+            // then
+            assertThatThrownBy(() ->
+                    githubRepoQuery.findByUserIdAndRepositoryId(userId, repositoryId))
+                    .isInstanceOf(GeneralException.class)
+                    .extracting("exception")
+                    .isEqualTo(GithubRepoException.UNAUTHORIZED_GITHUB_REPO_ACCESS);
         }
     }
 }
