0a268adac12ed3eb9cea3c3ef76ce6350d2f680f
dnjstjt1297 <dnjstjt1297@gmail.com>
2025-05-16 09:07:07 +0900

    fix: 삭제 잘못한 파일 다시 업로드 (#40)

---
+++ src/test/java/com/specialwarriors/conal/ConalApplicationTests.java
@@ -0,0 +1,30 @@
+package com.specialwarriors.conal;
+
+import com.specialwarriors.conal.common.auth.jwt.JwtTokenProvider;
+import com.specialwarriors.conal.util.MailUtil;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class ConalApplicationTests {
+
+    @MockitoBean
+    private WebClient githubRevokeWebClient;
+
+    @MockitoBean
+    private WebClient githubWebClient;
+
+    @MockitoBean
+    private JwtTokenProvider jwtTokenProvider;
+
+    @MockitoBean
+    private MailUtil mailUtil;
+
+    @Test
+    void contextLoads() {
+    }
+}
