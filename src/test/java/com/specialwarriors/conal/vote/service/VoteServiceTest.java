85b615f63e0cf13e53042d45a160bd77db9e0681
minjae <mj3242@naver.com>
2025-05-22 20:34:49 +0900

    test: 투표 폼 구성 데이터 제공 기능 단위 테스트 추가(#31)

--- src/test/java/com/specialwarriors/conal/vote/service/VoteServiceTest.java
+++ src/test/java/com/specialwarriors/conal/vote/service/VoteServiceTest.java
@@ -5,7 +5,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.when;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
@@ -16,11 +15,16 @@ import com.specialwarriors.conal.common.exception.GeneralException;
 import com.specialwarriors.conal.contributor.domain.Contributor;
 import com.specialwarriors.conal.github_repo.domain.GithubRepo;
 import com.specialwarriors.conal.github_repo.service.GithubRepoQuery;
+import com.specialwarriors.conal.vote.dto.response.VoteFormResponse;
 import com.specialwarriors.conal.vote.exception.VoteException;
+import io.jsonwebtoken.JwtException;
 import java.time.Duration;
+import java.util.Collections;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -36,7 +40,8 @@ import org.springframework.data.redis.core.SetOperations;
 class VoteServiceTest {
 
     private final long REPO_ID = 1L;
-    private final String EMAIL = "ex@example.com";
+    private final List<String> EMAILS = List.of("mj3242@naver.com", "mj1111@naver.com",
+            "mj2222@naver.com", "mj3333@naver.com");
     private final String USER_TOKEN = "header.payload.signature";
 
     @Mock
@@ -65,11 +70,14 @@ class VoteServiceTest {
             List<Contributor> contributors = IntStream.range(0, 4)
                     .mapToObj(i -> mock(Contributor.class))
                     .toList();
-            contributors.forEach(contributor -> when(contributor.getEmail())
-                    .thenReturn(EMAIL));
+            for (int i = 0; i < contributors.size(); i++) {
+                when(contributors.get(i).getEmail()).thenReturn(EMAILS.get(i));
+            }
+
             when(githubRepo.getContributors()).thenReturn(contributors);
 
-            when(jwtTokenProvider.createVoteUserToken(eq(EMAIL), any(Date.class), anyLong()))
+            when(jwtTokenProvider.createVoteUserToken(any(String.class),
+                    any(Date.class), anyLong()))
                     .thenReturn(USER_TOKEN);
 
             SetOperations<String, String> setOperations = mock(SetOperations.class);
@@ -81,7 +89,7 @@ class VoteServiceTest {
             // then
             verify(githubRepoQuery).findById(REPO_ID);
             verify(jwtTokenProvider, times(contributors.size()))
-                    .createVoteUserToken(eq(EMAIL), any(Date.class), anyLong());
+                    .createVoteUserToken(any(String.class), any(Date.class), anyLong());
             verify(setOperations).add(any(String.class), any(String[].class));
             verify(redisTemplate).expire(any(String.class), any(Duration.class));
         }
@@ -113,11 +121,12 @@ class VoteServiceTest {
             // given
             when(redisTemplate.hasKey(any(String.class))).thenReturn(true);
 
-            Set<String> userTokens = mock(Set.class);
+            Set<String> userTokens = IntStream.range(0, 4)
+                    .mapToObj(i -> USER_TOKEN)
+                    .collect(Collectors.toSet());
             SetOperations<String, String> setOperations = mock(SetOperations.class);
             when(redisTemplate.opsForSet()).thenReturn(setOperations);
             when(setOperations.members(any(String.class))).thenReturn(userTokens);
-            when(userTokens.contains(any(String.class))).thenReturn(true);
 
             GithubRepo githubRepo = mock(GithubRepo.class);
             when(githubRepoQuery.findById(REPO_ID)).thenReturn(githubRepo);
@@ -125,16 +134,17 @@ class VoteServiceTest {
             List<Contributor> contributors = IntStream.range(0, 4)
                     .mapToObj(i -> mock(Contributor.class))
                     .toList();
-            contributors.forEach(contributor -> when(contributor.getEmail())
-                    .thenReturn(EMAIL));
+            for (int i = 0; i < contributors.size(); i++) {
+                when(contributors.get(i).getEmail()).thenReturn(EMAILS.get(i));
+            }
             when(githubRepo.getContributors()).thenReturn(contributors);
 
             // when
             List<String> emails = voteService.findVoteTargetEmails(REPO_ID, USER_TOKEN);
 
             // then
-            assertThat(emails.size()).isEqualTo(contributors.size());
-            assertThat(emails).allMatch(email -> email.equals(EMAIL));
+            assertThat(emails).hasSize(contributors.size())
+                    .containsAll(EMAILS);
         }
 
         @DisplayName("투표 오픈 내역이 존재하지 않을 경우 예외가 발생한다.")
@@ -159,11 +169,10 @@ class VoteServiceTest {
             // given
             when(redisTemplate.hasKey(any(String.class))).thenReturn(true);
 
-            Set<String> userTokens = mock(Set.class);
+            Set<String> userTokens = Collections.EMPTY_SET;
             SetOperations<String, String> setOperations = mock(SetOperations.class);
             when(redisTemplate.opsForSet()).thenReturn(setOperations);
             when(setOperations.members(any(String.class))).thenReturn(userTokens);
-            when(userTokens.contains(any(String.class))).thenReturn(false);
 
             // when
 
@@ -196,6 +205,77 @@ class VoteServiceTest {
                     .isInstanceOf(GeneralException.class)
                     .extracting("exception")
                     .isEqualTo(GITHUB_REPO_NOT_FOUND);
+        }
+    }
+
+    @Nested
+    @DisplayName("투표 폼 구성 데이터 제공 시 ")
+    class GetVoteFormResponseTest {
+
+        @DisplayName("성공한다.")
+        @Test
+        public void success() {
+            // given
+            when(redisTemplate.hasKey(any(String.class))).thenReturn(true);
+
+            Set<String> userTokens = new HashSet<>();
+            for (int i = 0; i < EMAILS.size(); i++) {
+                String userToken = USER_TOKEN + i;
+                userTokens.add(userToken);
+                when(jwtTokenProvider.extractEmailFrom(userToken)).thenReturn(EMAILS.get(i));
+            }
+
+            SetOperations<String, String> setOperations = mock(SetOperations.class);
+            when(redisTemplate.opsForSet()).thenReturn(setOperations);
+            when(setOperations.members(any(String.class))).thenReturn(userTokens);
+
+            // when
+            List<VoteFormResponse> responses = voteService.getVoteFormResponse(REPO_ID);
+
+            // then
+            assertThat(responses).hasSize(userTokens.size())
+                    .allMatch(response -> response.repoId() == REPO_ID)
+                    .allMatch(response -> EMAILS.contains(response.email()))
+                    .allMatch(response -> userTokens.contains(response.userToken()))
+                    .allMatch(response -> EMAILS.containsAll(response.voteTargetEmails()));
+        }
+
+        @DisplayName("투표 오픈 내역이 존재하지 않을 경우 예외가 발생한다.")
+        @Test
+        public void voteNotFound() {
+            // given
+            when(redisTemplate.hasKey(any(String.class))).thenReturn(false);
+
+            // when
+
+            // then
+            assertThatThrownBy(() -> voteService.getVoteFormResponse(REPO_ID))
+                    .isInstanceOf(GeneralException.class)
+                    .extracting("exception")
+                    .isEqualTo(VoteException.VOTE_NOT_FOUND);
+        }
+
+        @DisplayName("유효하지 않은 사용자 토큰이 존재할 경우 예외가 발생한다.")
+        @Test
+        public void invalidUserToken() {
+            // given
+            when(redisTemplate.hasKey(any(String.class))).thenReturn(true);
+
+            Set<String> userTokens = IntStream.range(0, 4)
+                    .mapToObj(i -> USER_TOKEN + i)
+                    .collect(Collectors.toSet());
+            SetOperations<String, String> setOperations = mock(SetOperations.class);
+            when(redisTemplate.opsForSet()).thenReturn(setOperations);
+            when(setOperations.members(any(String.class))).thenReturn(userTokens);
+
+            when(jwtTokenProvider.extractEmailFrom(any(String.class)))
+                    .thenThrow(new JwtException("invalid user token"));
+
+            // when
+
+            // then
+            assertThatThrownBy(() -> voteService.getVoteFormResponse(REPO_ID))
+                    .isInstanceOf(JwtException.class);
         }
     }
 }
